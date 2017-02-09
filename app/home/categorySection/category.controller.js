bf561b41928e63a20ee5748c50f2557643bfb1db
EzequielGomezCucchiararo <ezequiel.cucchiararo@gmail.com>
2017-02-09 19:05:05 +0100

    sass files added

--- app/home/categorySection/category.controller.js
+++ app/home/categorySection/category.controller.js
@@ -10,38 +10,46 @@
     vm.categoriesOne = [
       {
         name: 'Adventure',
-        id: 12
+        id: 12,
+        poster: 'http://www.supernovarock.cl/wp-content/uploads/2013/03/gi01.jpg'
       },
       {
         name: 'Action',
-        id: 28
+        id: 28,
+        poster: 'http://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
       },
       {
         name: 'Family',
-        id: 10751
+        id: 10751,
+        poster: 'http://assets.nydailynews.com/polopoly_fs/1.5810.1313648894!/img/httpImage/sopranos.jpg'
       },
       {
         name: 'Horror',
-        id: 27
+        id: 27,
+        poster: 'https://s-media-cache-ak0.pinimg.com/736x/30/83/08/3083088424d1ab30945e004a557455b5.jpg'
       }
     ]
 
     vm.categoriesTwo = [
       {
         name: 'Animation',
-        id: 16
+        id: 16,
+        poster: 'http://www.superprod.net/system/events/images/104/fullscreen/LassieZoe.jpg'
       },
       {
         name: 'Comedy',
-        id: 35
+        id: 35,
+        poster: 'http://www.glamourhunt.com/images/Heyy_baby.jpg'
       },
       {
         name: 'Thriller',
-        id: 53
+        id: 53,
+        poster: 'https://www.moviesky.in/wp-content/uploads/2016/09/Dont-Breathe-2016-English-Movie-350x250.jpg'
       },
       {
         name: 'Romance',
-        id: 10749
+        id: 10749,
+        poster: 'http://www.geocities.ws/dirmanirathnam/iruvar1cl.jpg'
       }]
 
     // generate random num from 1 to 19
