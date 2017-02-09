8a5cd8016efd27363052c19d847d6e20c00a3454
bijay timilsina <bjtimi.007@gmail.com>
2017-02-09 20:32:35 +0100

    home factory changed with new filter methods

--- app/home/categorySection/category.factory.js
+++ app/home/categorySection/category.factory.js
@@ -10,14 +10,17 @@
 
     var objectReturn = {
       getMoviesByCategory: getMoviesByCategory,
-      getMovie: getMovie,
+      // getMovie: getMovie,
       getPopular: getPopular,
-      getLatest: getLatest,
       getTopRated: getTopRated,
       getNowPlaying: getNowPlaying,
-      getUpcoming: getUpcoming,
-      getIdByCategoryName: getIdByCategoryName,
-      getInfoMovie: getInfoMovie
+      getPopularGenre: getPopularGenre,
+      getUpcomingGenre: getUpcomingGenre,
+      getLatestGenre: getLatestGenre,
+      getTopRatedGenre: getTopRatedGenre,
+      getNowPlayingGenre: getNowPlayingGenre,
+      getInfoMovie: getInfoMovie,
+      getIdByCategoryName: getIdByCategoryName
     }
 
     function getMoviesByCategory (categoryID) {
@@ -28,42 +31,92 @@
         })
     }
 
-    function getMovie (categoryID) {
-      return $http.get('https://api.themoviedb.org/3/genre/' + categoryID + '/movies?api_key=' + apiKey + '&language=en-US&include_adult=false&sort_by=created_at.asc')
-    }
-
+    // function getMovie (categoryID) {
+    //   return $http.get('https://api.themoviedb.org/3/genre/' + categoryID + '/movies?api_key=' + apiKey + '&language=en-US&include_adult=false&sort_by=created_at.asc')
+    //     .then(function (response) {
+    //       return response.data.results
+    //     })
+    // }
+// ****** get most popular, upcoming and toprated of all movies for Carrusel in home page ***//
     function getPopular () {
-      return $http.get('https://api.themoviedb.org/3/movie/popular?api_key=' + apiKey + '')
-      .then(function (response) {
-        return response.data.results
-      })
-    }
-
-    function getUpcoming () {
-      return $http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=' + apiKey + '&language=en-US&page=1')
+      var url = 'https://api.themoviedb.org/3/movie/popular?api_key=' + apiKey + '&language=en-US'
+      return $http.get(url)
         .then(function (response) {
           return response.data.results
         })
     }
-
-    function getLatest () {
-      return $http.get('https://api.themoviedb.org/3/movie/latest?api_key=' + apiKey + '&language=en-US')
-      .then(function (response) {
-        return response.data.results
-      })
-    }
-
     function getTopRated () {
       return $http.get('https://api.themoviedb.org/3/movie/top_rated?api_key=' + apiKey + '&language=en-US&page=1')
       .then(function (response) {
         return response.data.results
       })
     }
-
     function getNowPlaying () {
       return $http.get('https://api.themoviedb.org/3/movie/now_playing?api_key=' + apiKey + '')
       .then(function (response) {
         return response.data.results
+      })
+    }
+
+//* ** popular movies of genre having id 'categoryID' for page 2 **//
+    function getPopularGenre (categoryID) {
+      var url = 'https://api.themoviedb.org/3/movie/popular?api_key=' + apiKey + '&language=en-US'
+      return $http.get(url)
+              .then(function (response) {
+                return response.data.results
+              })
+      .then(function (response) {
+        return response.filter(function (movie) {
+          return movie.genre_ids.indexOf(categoryID) !== -1
+        })
+      })
+    }
+
+    function getUpcomingGenre (categoryID) {
+      return $http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=' + apiKey + '&language=en-US&page=1')
+        .then(function (response) {
+          return response.data.results
+        })
+      .then(function (response) {
+        return response.filter(function (movie) {
+          return movie.genre_ids.indexOf(categoryID) !== -1
+        })
+      })
+    }
+
+    function getLatestGenre (categoryID) {
+      return $http.get('https://api.themoviedb.org/3/movie/latest?api_key=' + apiKey + '&language=en-US')
+      .then(function (response) {
+        return response.data.results
+      })
+      .then(function (response) {
+        return response.filter(function (movie) {
+          return movie.genre_ids.indexOf(categoryID) !== -1
+        })
+      })
+    }
+
+    function getTopRatedGenre (categoryID) {
+      return $http.get('https://api.themoviedb.org/3/movie/top_rated?api_key=' + apiKey + '&language=en-US&page=1')
+      .then(function (response) {
+        return response.data.results
+      })
+      .then(function (response) {
+        return response.filter(function (movie) {
+          return movie.genre_ids.indexOf(categoryID) !== -1
+        })
+      })
+    }
+
+    function getNowPlayingGenre (categoryID) {
+      return $http.get('https://api.themoviedb.org/3/movie/now_playing?api_key=' + apiKey + '')
+      .then(function (response) {
+        return response.data.results
+      })
+      .then(function (response) {
+        return response.filter(function (movie) {
+          return movie.genre_ids.indexOf(categoryID) !== -1
+        })
       })
     }
 
@@ -85,7 +138,7 @@
     //   //     categoryID = elem.id
     //   //   }
     //   // })
-
+      console.log(categoryID + ' is ID returned')
       return categoryID
     }
 
