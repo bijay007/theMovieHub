14bfe98a6c9e6f168faa3f2023243956d49531d7
EzequielGomezCucchiararo <ezequiel.cucchiararo@gmail.com>
2017-02-10 11:26:04 +0100

    category name modifed to show only letters

--- app/category/category.controller.js
+++ app/category/category.controller.js
@@ -7,6 +7,7 @@
     vm.moviePrePath = 'http://image.tmdb.org/t/p/w150'
     vm.categoryName = $routeParams.name
     vm.categoryID = parseInt(this.categoryName)
+    vm.categoryName = vm.categoryName.match(/[a-zA-Z]/g).join('')
     vm.query = ''
     movieHubFactory.getMoviesByCategory(this.categoryID)
     .then(function (data) {
