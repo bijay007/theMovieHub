7547267499fc5309b423852cc49b895f91e36872
bijay timilsina <bjtimi.007@gmail.com>
2017-02-10 11:35:17 +0100

    search bar removed in category templates

---
+++ toprated/toprated.controller.js
@@ -0,0 +1,21 @@
+/* eslint no-undef: 'off' */
+
+(function () {
+  angular.module('app')
+    .controller('TopRatedController', TopRatedController)
+
+  function TopRatedController ($routeParams, movieHubFactory) {
+    var vm = this
+    vm.moviePrePath = 'http://image.tmdb.org/t/p/w150'
+    vm.categoryName = $routeParams.name
+    vm.categoryID = parseInt(vm.categoryName)
+    vm.filterName = 'Top Rated'
+    vm.query = ''
+
+    movieHubFactory.getTopRatedGenre(vm.categoryID)
+    .then(function (data) {
+      vm.movies = data
+    })
+  }
+})()
+
---
+++ upcoming/upcoming.controller.js
@@ -0,0 +1,22 @@
+
+/* eslint no-undef: 'off' */
+
+(function () {
+  angular.module('app')
+    .controller('UpcomingController', UpcomingController)
+
+  function UpcomingController ($routeParams, movieHubFactory) {
+    var vm = this
+    vm.moviePrePath = 'http://image.tmdb.org/t/p/w150'
+    vm.categoryName = $routeParams.name
+    vm.categoryID = parseInt(vm.categoryName)
+    vm.filterName = 'Upcoming'
+    vm.query = ''
+
+    movieHubFactory.getUpcomingGenre(vm.categoryID)
+    .then(function (data) {
+      vm.movies = data
+    })
+  }
+})()
+
