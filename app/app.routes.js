b39e8bfde8ad1d5f8e4e5f279b65c76b97066adb
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-02-10 15:14:00 +0100

    paths changed

--- app/app.routes.js
+++ app/app.routes.js
@@ -2,42 +2,42 @@ angular.module('app')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/home', {
-        templateUrl: '/app/home/home.template.html',
+        templateUrl: 'app/home/home.template.html',
         controller: 'CategoryHomeController',
         controllerAs: 'vm'
       })
       .when('/category/:name', {
-        templateUrl: '/app/category/category.template.html',
+        templateUrl: 'app/category/category.template.html',
         controller: 'CategoryController',
         controllerAs: 'vm'
       })
       .when('/category/:name/popular', {
-        templateUrl: '/app/category/popular/popular.template.html',
+        templateUrl: 'app/category/popular/popular.template.html',
         controller: 'PopularController',
         controllerAs: 'vm'
       })
       .when('/category/:name/upcoming', {
-        templateUrl: '/app/category/upcoming/upcoming.template.html',
+        templateUrl: 'app/category/upcoming/upcoming.template.html',
         controller: 'UpcomingController',
         controllerAs: 'vm'
       })
       .when('/category/:name/latest', {
-        templateUrl: '/app/category/latest/latest.template.html',
+        templateUrl: 'app/category/latest/latest.template.html',
         controller: 'LatestController',
         controllerAs: 'vm'
       })
       .when('/category/:name/toprated', {
-        templateUrl: '/app/category/toprated/toprated.template.html',
+        templateUrl: 'app/category/toprated/toprated.template.html',
         controller: 'TopRatedController',
         controllerAs: 'vm'
       })
       .when('/category/:name/nowplaying', {
-        templateUrl: '/app/category/nowplaying/nowplaying.template.html',
+        templateUrl: 'app/category/nowplaying/nowplaying.template.html',
         controller: 'NowPlayingController',
         controllerAs: 'vm'
       })
        .when('/info_movie/:name', {
-         templateUrl: '/app/info_movie/info.movie.template.html',
+         templateUrl: 'app/info_movie/info.movie.template.html',
          controller: 'InfoMovieController',
          controllerAs: 'vm'
        })
