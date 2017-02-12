d302c7dd52efb413b896ffa721d799902576d709
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-02-12 09:51:44 +0100

    constants

---
+++ app/app.constants.js
@@ -0,0 +1,44 @@
+angular.module('app')
+  .constant('pathCoverGenre', 'https://bijay007.github.io/theMovieHub/assets/img/')
+  .constant('genresConfig', [
+    {
+      name: 'Adventure',
+      id: 12,
+      poster: 'https://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
+    },
+    {
+      name: 'Action',
+      id: 28,
+      poster: 'https://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
+    },
+    {
+      name: 'Family',
+      id: 10751,
+      poster: 'https://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
+    },
+    {
+      name: 'Horror',
+      id: 27,
+      poster: 'https://s-media-cache-ak0.pinimg.com/736x/30/83/08/3083088424d1ab30945e004a557455b5.jpg'
+    },
+    {
+      name: 'Animation',
+      id: 16,
+      poster: 'https://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
+    },
+    {
+      name: 'Comedy',
+      id: 35,
+      poster: 'https://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
+    },
+    {
+      name: 'Thriller',
+      id: 53,
+      poster: 'https://www.moviesky.in/wp-content/uploads/2016/09/Dont-Breathe-2016-English-Movie-350x250.jpg'
+    },
+    {
+      name: 'Romance',
+      id: 10749,
+      poster: 'https://www.geocities.ws/dirmanirathnam/iruvar1cl.jpg'
+    }]
+  )
--- app/home/categorySection/category.controller.js
+++ app/home/categorySection/category.controller.js
@@ -4,53 +4,10 @@
       .module('app')
       .controller('CategoryHomeController', CategoryHomeController)
 
-  function CategoryHomeController (movieHubFactory) {
+  function CategoryHomeController (movieHubFactory, genresConfig) {
     var vm = this
 
-    vm.categoriesOne = [
-      {
-        name: 'Adventure',
-        id: 12,
-        poster: 'https://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
-      },
-      {
-        name: 'Action',
-        id: 28,
-        poster: 'https://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
-      },
-      {
-        name: 'Family',
-        id: 10751,
-        poster: 'https://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
-      },
-      {
-        name: 'Horror',
-        id: 27,
-        poster: 'https://s-media-cache-ak0.pinimg.com/736x/30/83/08/3083088424d1ab30945e004a557455b5.jpg'
-      }
-    ]
-
-    vm.categoriesTwo = [
-      {
-        name: 'Animation',
-        id: 16,
-        poster: 'https://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
-      },
-      {
-        name: 'Comedy',
-        id: 35,
-        poster: 'https://openbooksociety.com/wp-content/uploads/HLIC/70720c8145c729c1512fd98bfd99323b.jpg'
-      },
-      {
-        name: 'Thriller',
-        id: 53,
-        poster: 'https://www.moviesky.in/wp-content/uploads/2016/09/Dont-Breathe-2016-English-Movie-350x250.jpg'
-      },
-      {
-        name: 'Romance',
-        id: 10749,
-        poster: 'https://www.geocities.ws/dirmanirathnam/iruvar1cl.jpg'
-      }]
+    vm.categoriesOne = genresConfig
 
     // generate random num from 1 to 19
 
