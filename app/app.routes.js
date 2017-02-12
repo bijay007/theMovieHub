527dc7f25a5c77a428cc31f7fb95b26643b13d32
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-02-12 09:49:25 +0100

    clean routes

--- app/app.routes.js
+++ app/app.routes.js
@@ -11,36 +11,10 @@ angular.module('app')
         controller: 'CategoryController',
         controllerAs: 'vm'
       })
-      .when('/category/:name/popular', {
-        templateUrl: 'app/category/popular/popular.template.html',
-        controller: 'PopularController',
+      .when('/info_movie/:name', {
+        templateUrl: 'app/info_movie/info.movie.template.html',
+        controller: 'InfoMovieController',
         controllerAs: 'vm'
       })
-      .when('/category/:name/upcoming', {
-        templateUrl: 'app/category/upcoming/upcoming.template.html',
-        controller: 'UpcomingController',
-        controllerAs: 'vm'
-      })
-      .when('/category/:name/latest', {
-        templateUrl: 'app/category/latest/latest.template.html',
-        controller: 'LatestController',
-        controllerAs: 'vm'
-      })
-      .when('/category/:name/toprated', {
-        templateUrl: 'app/category/toprated/toprated.template.html',
-        controller: 'TopRatedController',
-        controllerAs: 'vm'
-      })
-      .when('/category/:name/nowplaying', {
-        templateUrl: 'app/category/nowplaying/nowplaying.template.html',
-        controller: 'NowPlayingController',
-        controllerAs: 'vm'
-      })
-       .when('/info_movie/:name', {
-         templateUrl: 'app/info_movie/info.movie.template.html',
-         controller: 'InfoMovieController',
-         controllerAs: 'vm'
-       })
-
       .otherwise('/home')
   })
