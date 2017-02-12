928270c3ffa617c8c24f9a0dcd648e09d0f75934
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-02-12 10:28:22 +0100

    reorganized folders and renamed files

--- app/app.routes.js
+++ app/app.routes.js
@@ -2,17 +2,17 @@ angular.module('app')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/home', {
-        templateUrl: 'app/sections/home/home.template.html',
+        templateUrl: 'app/sections/home/template.html',
         controller: 'CategoryHomeController',
         controllerAs: 'vm'
       })
       .when('/category/:name', {
-        templateUrl: 'app/sections/category/category.template.html',
+        templateUrl: 'app/sections/category/template.html',
         controller: 'CategoryController',
         controllerAs: 'vm'
       })
       .when('/info_movie/:name', {
-        templateUrl: 'app/sections/info_movie/info.movie.template.html',
+        templateUrl: 'app/sections/info_movie/template.html',
         controller: 'InfoMovieController',
         controllerAs: 'vm'
       })
